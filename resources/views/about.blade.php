a8b707fc3077a129902ab3f283fbed2e2b045d36
Fateen Alzubi <Fateen980@users.noreply.github.com>
2025-06-12 21:38:22 +0300

    first commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -54,13 +54,13 @@
 
 
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BYQM5GL4TF"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QJFQKBTE7Z"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-BYQM5GL4TF');
+  gtag('config', 'G-QJFQKBTE7Z');
 </script>
 
 
