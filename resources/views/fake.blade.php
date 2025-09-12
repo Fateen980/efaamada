a966f510d30bbc256e3e539a962f38c37f132d8d
Fateen Alzubi <Fateen980@users.noreply.github.com>
2025-09-12 14:45:09 +0300

    message

--- resources/views/fake.blade.php
+++ resources/views/fake.blade.php
@@ -1,15 +1,44 @@
-<!-- LOCALIZERS: Each localizable piece of the page is marked with a comment -->
-<html>
-<head>
-    <link rel=stylesheet type="text/css" href="page-load-errors.css">
-    <!-- LOCALIZERS: You might want to change the font family. You can also add styles to override sizes, etc. -->
-    <style>
-        body {font-family:'-apple-system-font';}
-    </style>
-    <!-- LOCALIZERS: The next line contains the page title that appears in the window's title bar -->
-    <title>Failed to open page</title>
 
- <!-- Google tag (gtag.js) -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>404 HTML Template by Colorlib</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet"> 
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="../css/style.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+<meta name="robots" content="noindex, follow">
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>4<span>0</span>4</h1>
+			</div>
+			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+			<a href="#">home page</a>
+		</div>
+	</div>
+
+    <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QJFQKBTE7Z"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -27,20 +56,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TN55RKQS');</script>
 <!-- End Google Tag Manager -->
-</head>
-
-<body>
-    <div class="content-container">
-        <div class="error-container">
-            <div class="text-container">
-                <!-- Main title here. -->
-                <p class="error-title"><h3> Can’t Find the Server</h3></p>
-            </div>
-            <div class="text-container">
-
-            </div>
-        </div>
-    </div>
-</body>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
 </html>
