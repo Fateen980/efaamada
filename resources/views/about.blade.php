07fb7d23d1594aeabe64dc3e3b3f0756af9c056e
Fateen Alzubi <Fateen980@users.noreply.github.com>
2025-11-29 22:51:04 -0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="../new/merged.css" type="text/css" />
 
     <title>
-            المنصة الوطنية للمخالفات | من نحن 
+          المنصة الوطنية للمخالفات| منصة ايفاء|من نحن 
     </title>
     
 
