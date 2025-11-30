22802716f71498f9b6832e5b47c4f6d59cda01bd
Fateen Alzubi <Fateen980@users.noreply.github.com>
2025-11-30 10:05:12 -0800

    Update ContactUs.blade.php

--- resources/views/ContactUs.blade.php
+++ resources/views/ContactUs.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="../new/merged.css" type="text/css" />
 
     <title>
-    	المنصة الوطنية للمخالفات |منصة ايفاء|اتصل بنا
+        المنصة الوطنية للمخالفات (منصةإيفاء) | اتصل بنا
     </title>
     
 
