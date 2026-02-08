43fb803e98f06b5ed6b9ee6c166541e233c24ea9
Fateen <fateen@Fateens-MacBook-Air.local>
2026-02-08 15:26:14 -0800

    first

--- app/Http/Controllers/EfaaController.php
+++ app/Http/Controllers/EfaaController.php
@@ -21,7 +21,7 @@ class EfaaController extends Controller
     public function home(){
 
 
-        $STOP_THE_SYSTEM = $_ENV['STOP_THE_SYSTEM'];
+        $STOP_THE_SYSTEM = env('STOP_THE_SYSTEM',false);
      
 
 
