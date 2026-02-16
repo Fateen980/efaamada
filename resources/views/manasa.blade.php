dd337a1069b0046247f26d865ba0ed8206fba423
Fateen <fateen@Fateens-MacBook-Air.local>
2026-02-16 08:05:50 -0800

    first commit

--- resources/views/manasa.blade.php
+++ resources/views/manasa.blade.php
@@ -6,17 +6,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" type="image/x-icon" href="https://cdn.jsdelivr.net/gh/Fateen980/efaa@main/public/favicon.ico" />
     <meta name="author"/>
-    <meta name="keywords" content="المنصة الوطنية للمخالفات (إيفاء)"/>
-    <meta name="description" content="المنصة الوطنية للمخالفات (إيفاء) هي إحدى المنصات الوطنية التي ينفذها مركز المعلومات الوطني التابع للهيئة السعودية للبيانات والذكاء الإصطناعي تخفيض (50%) من قيمة المخالفة / المخالفات"/>
-    <meta property="og:title" content="المنصة الوطنية للمخالفات (إيفاء)"/>
+    <meta name="keywords" content="منصة ايفاء"/>
+    <meta name="description" content="منصة ايفاء هي إحدى المنصات الوطنية التي ينفذها مركز المعلومات الوطني التابع للهيئة السعودية للبيانات والذكاء الإصطناعي تخفيض (50%) من قيمة المخالفة / المخالفات"/>
+    <meta property="og:title" content="منصة ايفاء"/>
     <meta property="og:type" content="website"/>
     <meta property="og:url"/>
     <meta property="og:image"/>
-    <meta property="og:site_name" content="المنصة الوطنية للمخالفات (إيفاء)"/>
-    <meta property="og:description" content="المنصة الوطنية للمخالفات (إيفاء) هي إحدى المنصات الوطنية التي ينفذها مركز المعلومات الوطني التابع للهيئة السعودية للبيانات والذكاء الإصطناعي تخفيض (50%) من قيمة المخالفة / المخالفات"/>
+    <meta property="og:site_name" content="منصة ايفاء"/>
+    <meta property="og:description" content="منصة ايفاء هي إحدى المنصات الوطنية التي ينفذها مركز المعلومات الوطني التابع للهيئة السعودية للبيانات والذكاء الإصطناعي تخفيض (50%) من قيمة المخالفة / المخالفات"/>
     <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:title" content="المنصة الوطنية للمخالفات (إيفاء)"/>
-    <meta name="twitter:description" content="المنصة الوطنية للمخالفات (إيفاء) هي إحدى المنصات الوطنية التي ينفذها مركز المعلومات الوطني التابع للهيئة السعودية للبيانات والذكاء الإصطناعي تخفيض (50%) من قيمة المخالفة / المخالفات"/>
+    <meta name="twitter:title" content="منصة ايفاء"/>
+    <meta name="twitter:description" content="منصة ايفاء هي إحدى المنصات الوطنية التي ينفذها مركز المعلومات الوطني التابع للهيئة السعودية للبيانات والذكاء الإصطناعي تخفيض (50%) من قيمة المخالفة / المخالفات"/>
     <meta name="twitter:image"/>
     <meta name="google-site-verification" content="DrRdpb3VsERdPP8dQtHz0tjgw6sr0KSHqjoSuxiMC-E" />
    
